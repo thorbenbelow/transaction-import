bbd8553b161bcb0481d5eb120972296b69d2cdd7
Thorben Below <thorbenbelow@gmail.com>
2022-09-28 23:26:40 +0200

    add custom css

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import transactionRouter from "./api/transaction"
 import {db} from "./prisma";
+import bodyParser from "body-parser";
 
 const app = express()
 
-
+app.use(express.static("public"))
+app.use(bodyParser.json())
 app.set('view engine', 'pug')
 
 app.use('/t', transactionRouter)
