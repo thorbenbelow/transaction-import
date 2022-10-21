435ca1a5be68d6ec39087a7c63501279f2d7209d
Thorben Below <thorbenbelow@gmail.com>
2022-10-21 21:05:28 +0200

    refactoring

--- src/app.ts
+++ src/app.ts
@@ -2,21 +2,23 @@ import express from 'express'
 import transactionRouter from "./router/transaction-router"
 import labelRouter from "./router/label-router";
 import bodyParser from "body-parser";
-import {getTransactions} from "./service/transaction-service";
+import {Route} from "./router/routes";
+import morgan from "morgan"
 
 const app = express()
 
 app.use(express.static("public"))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(morgan("tiny"))
 app.set('view engine', 'pug')
 
-app.use('/t', transactionRouter)
-app.use('/label', labelRouter)
+
+app.use(Route.Transaction, transactionRouter)
+app.use(Route.Label, labelRouter)
 
 app.get('/', async (req, res) => {
-    const transactions = await getTransactions();
-    res.render('transaction_list', {transactions})
+    res.redirect(Route.Transaction)
 })
 
 export default app
